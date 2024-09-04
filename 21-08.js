74cd3ed98109729a1f38d3168fd17d216d7a8222
Monisha <monishasharma134@gmail.com>
2024-09-04 09:59:04 +0530

    first commit

---
+++ 21-08.js
@@ -0,0 +1,29 @@
+function show(){
+    alert("this is an external js file");
+    /*
+    variables start w a-z A-Z $ or _ and only contain a-z A-Z $ _
+    case-sensitive
+    declared usuign :
+        1.var — The most common variable. It can be reassigned but only accessed 
+        within  a  function.  Variables  defined  with  var  move  to  the  top  when  the 
+        code is executed. 
+        2.  const — Cannot be reassigned and not accessible before they appear 
+        within the code. 
+        3.  let — Similar to const, the let variable can be reassigned but not re- 
+        declared.
+    datatypes-
+        primitive- number str bool nll undef
+        non primi (with new keyword)-object number str date arr bool 
+            eg -
+            new Object();
+            var accno = new Array(5);
+            var nm= new String ("asb");
+        almost everything is an object - date math reg expre arrat func always objects
+    operators-
+        samesame
+        overloading/overriding
+        + concatenates if even 1 is string
+    constructs if else while for switch case
+    
+    */
+}
